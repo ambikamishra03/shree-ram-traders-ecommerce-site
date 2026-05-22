39a254a12c770efa7d66fcf22bf3cbb88dbeba1c
Ambika Mishra <ambikamishra9236@gmail.com>
2026-05-22 12:40:04 +0530

    Frontend Folder structure ready

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home() {
+
+  return (
+    <>
+    <div>This is home page, this will shown to user first.</div>
+     {/* Navbar
+     each product with image price and quantity
+     footer */}
+    </>
+  )
+}
+
+export default Home
